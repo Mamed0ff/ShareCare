449e2d3da5c1b413a014fe74f1c7a9965991cbc5
Mamed0ff <rizvan.memmedov2004@gmail.com>
2024-09-23 01:59:34 +0400

    Dto

--- src/main/java/az/rentall/mvp/model/dto/request/FaqsRequest.java
+++ src/main/java/az/rentall/mvp/model/dto/request/FaqsRequest.java
@@ -1,5 +1,9 @@
 package az.rentall.mvp.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,5 +11,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class FaqsRequest {
+
+    @NotNull(message ="Question cannot be empty")
+    @Size(min = 2, max = 255,message = "Question length must be in 2-255 characters")
+    private String question;
+
+    @NotBlank(message = "Answer cannot be empty")
+    private String answer;
+
+    @NotNull(message = "category must be selected")
+    private Long categoryId;
 }
