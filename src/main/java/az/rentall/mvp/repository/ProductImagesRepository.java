2e56093bf814b48065bf1113e757cddddd3b167f
Mamed0ff <rizvan.memmedov2004@gmail.com>
2024-10-01 01:04:16 +0400

    Product image handling is finished

--- src/main/java/az/rentall/mvp/repository/ProductImagesRepository.java
+++ src/main/java/az/rentall/mvp/repository/ProductImagesRepository.java
@@ -3,5 +3,8 @@ package az.rentall.mvp.repository;
 import az.rentall.mvp.model.entity.ProductImages;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductImagesRepository extends JpaRepository<ProductImages, Long> {
+    public List<ProductImages>  findByProductIdAndStatus(Long productID, Boolean status);
 }
