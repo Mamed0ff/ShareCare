66f932a7a0d4875e16127fc170dbad383e18dde7
nasib-514 <basqalli14@gmail.com>
2024-10-10 08:44:27 +0400

    Were added EmailSender config, Multipart to createProduct and updateProduct methods and findAllProducts were written with Pageable

---
+++ src/main/java/az/rentall/mvp/config/EmailSenderConfig.java
@@ -0,0 +1,41 @@
+package az.rentall.mvp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailSenderConfig {
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailHost);
+        mailSender.setPort(587);
+
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+
+        return mailSender;
+
+    }
+}
--- src/main/java/az/rentall/mvp/controller/ProductController.java
+++ src/main/java/az/rentall/mvp/controller/ProductController.java
@@ -3,8 +3,15 @@ package az.rentall.mvp.controller;
 import az.rentall.mvp.model.dto.request.ProductRequest;
 import az.rentall.mvp.model.dto.response.ProductResponse;
 import az.rentall.mvp.service.ProductService;
+import az.rentall.mvp.util.annotation.ValidImages;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -19,7 +26,8 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@RequestPart("productRequest") @Valid ProductRequest productRequest,
+                                         @RequestPart("images") @ValidImages List<MultipartFile> images) {
         return productService.createProduct(productRequest);
     }
 
@@ -29,12 +37,13 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ProductResponse> findAllProducts(){
-        return productService.findAllProducts();
+    public Page<ProductResponse> findAllProducts(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
+        return productService.findAllProducts((java.awt.print.Pageable) pageable);
     }
 
     @PutMapping("/{id}")
-    public void updateProduct (@RequestBody ProductRequest productRequest, @PathVariable Long id){
+    public void updateProduct (@RequestPart("productRequest") @Valid ProductRequest productRequest,
+                               @RequestPart("images") @ValidImages List<MultipartFile> images, @PathVariable Long id){
         productService.updateProduct(productRequest, id);
     }
 }
