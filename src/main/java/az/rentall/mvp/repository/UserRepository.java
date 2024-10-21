bc93aed1b2f6e085fda037ebda31239a484c87af
Mamed0ff <rizvan.memmedov2004@gmail.com>
2024-10-22 00:11:20 +0400

    Security started: JWT token service created.

--- src/main/java/az/rentall/mvp/repository/UserRepository.java
+++ src/main/java/az/rentall/mvp/repository/UserRepository.java
@@ -5,7 +5,11 @@ import az.rentall.mvp.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    Boolean existsByEmail(String email);
 
 }
