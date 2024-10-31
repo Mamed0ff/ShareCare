37c1ab3e5cc5417be8e23622ffcadee4b4eff69d
nasib-514 <basqalli14@gmail.com>
2024-10-31 21:13:48 +0400

    AuthService, AuthController

--- src/main/java/az/rentall/mvp/controller/AuthController.java
+++ src/main/java/az/rentall/mvp/controller/AuthController.java
@@ -1,11 +1,53 @@
 package az.rentall.mvp.controller;
 
+import az.rentall.mvp.model.dto.request.ResetPassword;
+import az.rentall.mvp.model.dto.request.UserLoginRequest;
+import az.rentall.mvp.model.dto.request.UserRegisterRequest;
+import az.rentall.mvp.model.dto.request.VerificationRequest;
+import az.rentall.mvp.model.dto.response.JwtResponse;
+import az.rentall.mvp.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody UserRegisterRequest userRegisterRequest) {
+        ResponseEntity.ok(userRegisterRequest);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody UserLoginRequest userLoginRequest) {
+        return ResponseEntity.ok(authService.login(userLoginRequest));
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyAccount(@RequestBody VerificationRequest verificationRequest) {
+        authService.verifyAccount(verificationRequest);
+        return new ResponseEntity<>("Account verified successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/resend-verification")
+    public ResponseEntity<String> resendVerificationCode(@RequestParam String email) {
+        authService.resendVerificationCode(email);
+        return new ResponseEntity<>("Verification code sent successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/update-password")
+    public ResponseEntity<String> updatePassword(@RequestParam String email) {
+        authService.updatePassword(email);
+        return new ResponseEntity<>("Password updated successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> resetPassword(@RequestParam String token, @RequestBody ResetPassword resetPassword) {
+        authService.resetPassword(token, resetPassword);
+        return new ResponseEntity<>("Password reset successfully", HttpStatus.OK);
+    }
 }
--- src/main/java/az/rentall/mvp/service/impl/AuthServiceImpl.java
+++ src/main/java/az/rentall/mvp/service/impl/AuthServiceImpl.java
@@ -1,17 +1,22 @@
 package az.rentall.mvp.service.impl;
 
+import az.rentall.mvp.model.Enums.RoleType;
 import az.rentall.mvp.model.dto.request.ResetPassword;
 import az.rentall.mvp.model.dto.request.UserLoginRequest;
 import az.rentall.mvp.model.dto.request.UserRegisterRequest;
 import az.rentall.mvp.model.dto.request.VerificationRequest;
 import az.rentall.mvp.model.dto.response.JwtResponse;
+import az.rentall.mvp.model.entity.UserEntity;
 import az.rentall.mvp.repository.UserRepository;
 import az.rentall.mvp.service.AuthService;
 import az.rentall.mvp.service.EmailSenderService;
 import az.rentall.mvp.service.jwt.JwtService;
+import az.rentall.mvp.service.jwt.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,37 +27,81 @@ public class AuthServiceImpl implements AuthService {
 
     private final JwtService jwtService;
     private final EmailSenderService mailService;
-    private final UserRepository userRepository;
+    private final UserDetailsImpl userDetails;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
-    @Override
-    public void register(UserRegisterRequest request) {
+    private final UserRepository repository;
 
+    private final PasswordEncoder encoder;
+
+    private final AuthenticationManager authManager;
+
+    @Override
+    public void register(UserRegisterRequest userRegisterRequest) {
+        //Register the user to repository and generate a token
+        var user = UserEntity.builder()
+                .username(userRegisterRequest.getUsername())
+                .email(userRegisterRequest.getEmail())
+                .password(encoder.encode(userRegisterRequest.getPassword()))
+                .roleType(RoleType.USER)
+                .build();
+        repository.save(user);
+
+        var jwtToken = jwtService.generateAccessToken((UserDetails) user);
+        JwtResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     @Override
-    public JwtResponse login(UserLoginRequest request) {
-        return null;
+    public JwtResponse login(UserLoginRequest userLoginRequest) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userLoginRequest.getEmail(),
+                        userLoginRequest.getPassword()
+                )
+        );
+
+        var user = repository.findByEmail(userLoginRequest.getEmail()).orElseThrow();
+
+        var jwtToken = jwtService.generateAccessToken((UserDetails) user);
+        return JwtResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     @Override
-    public void verifyAccount(VerificationRequest request) {
-
+    public void verifyAccount(VerificationRequest verificationRequest) {
+        var user = repository.findByEmail(verificationRequest.getEmail()).orElseThrow();
+        if (jwtService.validateToken(verificationRequest.getEmail())) {
+            user.getIsVerified();
+            repository.save(user);
+        }
     }
 
     @Override
     public void resendVerificationCode(String email) {
-
+        var user = repository.findByEmail(email).orElseThrow();
+        var verificationToken = jwtService.generateAccessToken((UserDetails) user);
+        mailService.sendEmail(user.getEmail(), "subject", "verification code");
     }
 
     @Override
     public void updatePassword(String email) {
-
+        var user = repository.findByEmail(email).orElseThrow();
+        var newPassword = passwordEncoder.encode("newPassword"); // Сгенерируйте или получите новый пароль
+        user.setPassword(newPassword);
+        repository.save(user);
     }
 
     @Override
     public void resetPassword(String token, ResetPassword resetPassword) {
-
+        if (jwtService.validateToken(token)) {
+            var user = repository.findByEmail(resetPassword.getEmail()).orElseThrow();
+            user.setPassword(passwordEncoder.encode(resetPassword.getPassword()));
+            repository.save(user);
+        }
     }
 }
+
