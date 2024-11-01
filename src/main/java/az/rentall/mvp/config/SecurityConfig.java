2743556ad0fef83b0ed263788ae9cea5f31a3ab9
Mamed0ff <rizvan.memmedov2004@gmail.com>
2024-11-01 13:51:54 +0400

    cors disabled

--- src/main/java/az/rentall/mvp/config/SecurityConfig.java
+++ src/main/java/az/rentall/mvp/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request-> request
                         .requestMatchers(AUTH_WHITELIST).permitAll()
